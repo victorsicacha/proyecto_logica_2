0ee2d88a8e0917035aeb6bc14a975db7edf54df1
victorsicacha <55368819+victorsicacha@users.noreply.github.com>
2019-11-14 23:31:53 -0500

    Add files via upload

---
+++ DPLL-AV.py
@@ -0,0 +1,116 @@
+def clausula_unitaria(U):
+    flag = False
+    posicion = -1
+    for i in range(len(U)):
+        if(len(U[i]) == 0):
+            return (True, False, posicion)
+        elif(len(U[i]) == 1):
+            flag = True
+            posicion = i
+            break
+    return(False, flag, posicion)
+
+def claUnit(S):
+    for i in S:
+        if(len(i) == 1):
+            return True
+    return False
+
+def Complemento(l):
+    if(l[0] == '-'):
+        return l.replace('-', '')
+    else:
+        return '-' + l
+
+def quitar_clausula(S, l):
+    S.remove(l)
+    if(len(l) >= 1):
+        if(l[0] == '-'):
+            L = l.replace('-', '')
+        else:
+            L = l[0]
+        for i in S:
+            if L in i:
+                S.remove(i)
+    return S
+
+def quita_negacion(S, l):
+    if(len(l) >= 1):
+        L = l[0]
+        L = Complemento(L)
+        for i in S:
+            if L in i:
+                i.remove(L)
+    return S
+
+def unitPropagate(clausulas, Interpretacion):
+    vacia, unitaria, posicion = clausula_unitaria(clausulas)
+    while(vacia == False and claUnit(clausulas)):
+        for i in clausulas:
+            clausulas = quitar_clausula(clausulas, i)
+            clausulas = quita_negacion(clausulas, i)
+            if(len(i) == 0):
+                return 'Insatisfacible',  Interpretacion
+            if(i[0] == '-'):
+                 Interpretacion[Complemento(i[0])] = 0
+            else:
+                 Interpretacion[i[0]] = 1
+    return clausulas,  Interpretacion
+
+def lDicc(D):
+    A = D.copy()
+    for i in A:
+        if(i[0] == '-'):
+            A.pop(i)
+            i = Complemento(i)
+            if(A.get(i) == 0):
+                A[i[0]] = 1
+            else:
+                A[i[0]] = 0
+    return A
+
+def DPLL(S, I):
+    S, I = unitPropagate(S, I)
+    # print(S, I)
+    if(S == "Insatisfacible"):
+        return 'Insatisfacible', "{}"
+    if(len(S) == 0):
+        return 'Satisfacible', lDicc(I)
+    for i in S:
+        if(len(i) == 0):
+            return 'Insatisfacible', "{}"
+    L = S[0]
+    L = Complemento(Complemento(L[0]))
+    cop = I.copy()
+    if(L[0] == '-'):
+        cop[Complemento(L[0])] = 0
+    else:
+        cop[L[0]] = 1
+
+    Sp = S.copy()
+    Sp.append(L[0])
+
+    aux1, aux2 = DPLL(Sp, cop)
+    if(aux1 == 'Satisfacible'):
+        return 'Satisfacible', lDicc(cop)
+    else:
+        scop = S.copy()
+        scop.append(Complemento(L[0]))
+        icopy = I.copy()
+        if(L[0] == '-'):
+            icopy[Complemento(L[0])] = 1
+        else:
+            icopy[L[0]] = 0
+    return DPLL(scop, icopy)
+#dpll recive conjuntos de literales sin dobles negaciones y retorna la primera y solo una implementacion del valor de verdad de cada literalen un diccionario
+
+prueba = [["p", "-q", "r"], ["-p", "q", "-r"], ["-p", "-q", "r"], ["-p", "-q", "-r"]]
+#prueba2 = [["p"], ["-p", "q", "-r"], ["q"]]
+prueba4= [['-u'],[ '-v'],[ 'w'], ['-x'], [ '-y '],[ '-z']]#prueba para la regla de que soo una puerta tiene detras el diploma 
+#en este caso la unica manera de que sea verdadera es que el valor de verdad de mi W sea igual a 1___{'w': 1, 'u': 0, 'y': 0, 'v': 0, 'z': 0, 'x': 0}
+
+
+#prueba3 = [["p"], ["-p", "q"], ["-q", "r", "s"]]
+interpretaciones = {}
+msg, dicc_inter = DPLL(prueba4, interpretaciones)
+print(msg, dicc_inter)
---
+++ FNC-PROYECT-AV.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#letrasPoposicionales = ['u', 'v', 'w', 'x']
+#conectivos = ['Y', 'O', '>']
+
+# Subrutinas para la transformacion de una formula a su formula clausal
+
+# Subrutina de Tseitin para encontrar la FNC de la formula en la pila
+# Input: A (cadena) de la forma
+#                   p=-q
+#                   p=(qYr)
+#                   p=(qOr)
+#                   p=(q>r)
+# Output: B (cadena), equivalente en FNC
+def enFNC(A):
+    assert(len(A)==4 or len(A)==7), u"Fórmula incorrecta!"
+    B = ''
+    p = A[0]
+    # print('p', p)
+    if "-" in A:
+        q = A[-1]
+        # print('q', q)
+        B = "-"+p+"O-"+q+"Y"+p+"O"+q
+    elif "Y" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = q+"O-"+p+"Y"+r+"O-"+p+"Y-"+q+"O-"+r+"O"+p
+    elif "O" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = q+"O"+p+"Y-"+r+"O"+p+"Y"+q+"O"+r+"O-"+p
+    elif ">" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = q+"O"+p+"Y-"+r+"O"+p+"Y-"+q+"O"+r+"O-"+p
+    else:
+        print(u'Error enENC(): Fórmula incorrecta!')
+
+    return B
+
+# Algoritmo de transformacion de Tseitin
+# Input: A (cadena) en notacion inorder
+# Output: B (cadena), Tseitin
+    
+def Tseitin(A, letrasProposicionalesA):
+    letrasProposicionalesB = [chr(x) for x in range(256, 300)]
+    assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
+    L = []
+    Pila = []
+    i = -1
+    s = A[0]
+    while len(A) > 0:
+        if len(s) == 1 and len(Pila) > 0 and Pila[-1] == "-":
+            i += 1
+            Atomo = letrasProposicionalesB[i]
+            Pila.append(Atomo)
+            L.append(Atomo + "=" + "-" + s)
+            A = A[1:]
+        elif s == ")":
+            w = Pila[-1]
+            O = Pila[-2]
+            v = Pila[-3]
+            Pila = Pila[:len(Pila)-4]
+            i += 1
+            Atomo = letrasProposicionalesB[i]
+            L.append(Atomo + "=" + "(" + v + O + w + ")")
+            s = Atomo
+        else:
+            Pila.append(s)
+            A = A[1:]
+            if len(A) > 0:
+                s = A[0]
+    B = ""
+    if i < 0:
+        Atomo = Pila[-1]
+    else:
+        Atomo = letrasProposicionalesB[i]
+    for X in L:
+        Y = enFNC(X)
+        B += "Y" + Y
+    B = Atomo + B
+    return B
+    return "OK"
+
+# Subrutina Clausula para obtener lista de literales
+# Input: C (cadena) una clausula
+# Output: L (lista), lista de literales
+def Clausula(C):
+    L = []
+    while len(C) > 0:
+        s = C[0]
+        if s == "O":
+            C = C[1:]
+        elif s == "-":
+            literal = s + C[1]
+            L.append(literal)
+            C = C[2:]
+        else:
+            L.append(s)
+            C = C[1:]
+    return L
+    return "OK"
+
+# Algoritmo para obtencion de forma clausal
+# Input: A (cadena) en notacion inorder en FNC
+# Output: L (lista), lista de listas de literales
+def formaClausal(A):
+    L = []
+    i = 0
+    while len(A) > 0:
+        if i >= len(A):
+            L.append(Clausula(A))
+            A = []
+        else:
+            if A[i] == "Y":
+                L.append(Clausula(A[:i]))
+                A = A[i + 1:]
+                i = 0
+            else:
+                i += 1
+    return L
+    return "OK"
+
+#Test enFNC()
+#Descomente el siguiente código y corra el presente archivo
+formula = "p=(qYr)"
+print(enFNC(formula)) # Debe obtener qO-pYrO-pY-qO-rOp
+#Test Tseitin()
+#Descomente el siguiente código y corra el presente archivo
+letrasProposicionalesA = ['p', 'q']
+formula = "(pYq)"
+print(Tseitin(formula, letrasProposicionalesA)) # Debe obtener AYpO-AYqO-AY-pO-qOA (la A tiene una raya encima)
+#
+#Test Clausula()
+#Descomente el siguiente código y corra el presente archivo
+c = "pO-qOr"
+print(Clausula(c)) # Debe obtener ['p', '-q', 'r']
+#
+#Test formaClausal()
+#Descomente el siguiente código y corra el presente archivo
+f = "pO-qOrY-sOt"
+print(formaClausal(f)) # Debe obtener [['p', '-q', 'r'], ['-s', 't']]
